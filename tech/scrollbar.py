64e3d7c11a0d73a2c330386f6d6dbe8cf9a99ba7
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-20 16:35:49 +0800

    main

--- tech/scrollbar.py
+++ tech/scrollbar.py
@@ -9,60 +9,73 @@ window_title = pygame.display.set_caption('test')
 clock = pygame.time.Clock()
 pygame.display.flip()
 
-line_color = [255, 255, 255]
-line_pos = [180, 100]
-line_size = [10, 350]
+scrollbar_line_color = [255, 255, 255]
+scrollbar_line_pos = [180, 100]
+scrollbar_line_size = [10, 350]
 
-button_color = [161, 136, 127]
-button_pos = [172, 100]
-button_size = [25, 50]
+scrollbar_button_color = [161, 136, 127]
+scrollbar_button_pos = [172, 100]
+scrollbar_button_size = [25, 50]
 
-moveable = False
-move = 0
+scrollbar_moveable = False
+scrollbar_move = 0
 
 mouse_x, mouse_y = 0,0
 mouse_pos_x,mouse_pos_y = 0,0
 
-while True:
+def scrollbar_h_surface():
 
-    if move <= 0:
-        move = 0
-    if move >= 350-button_size[1]:
-        move = 350-button_size[1]
+    global scrollbar_move,scrollbar_line_size,scrollbar_button_size
+
+    if scrollbar_move <= 0:
+        scrollbar_move = 0
+    if scrollbar_move >= scrollbar_line_size[1]-scrollbar_button_size[1]:
+        scrollbar_move = scrollbar_line_size[1]-scrollbar_button_size[1]
 
     mainwindow.fill((0,0,0))
 
-    pygame.draw.rect(mainwindow, line_color,(line_pos,line_size), width=0)
-    pygame.draw.rect(mainwindow, button_color,(button_pos[0],button_pos[1]+move,button_size[0],button_size[1]), width=0)
+    pygame.draw.rect(mainwindow, scrollbar_line_color,(scrollbar_line_pos,scrollbar_line_size), width=0)
+    pygame.draw.rect(mainwindow, scrollbar_button_color,(scrollbar_button_pos[0],scrollbar_button_pos[1]+scrollbar_move,scrollbar_button_size[0],scrollbar_button_size[1]), width=0)
     
     pygame.display.update()
+
+    return scrollbar_move,scrollbar_line_size,scrollbar_button_size
+
+while True:
+
+    scrollbar_h_surface()
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+        '''
+
         if event.type == pygame.MOUSEMOTION:
 
             mouse_pos_x,mouse_pos_y = event.pos
-            if moveable == True:
-                move = mouse_pos_y - mouse_y
+            if scrollbar_moveable == True:
+                scrollbar_move = mouse_pos_y - mouse_y
+
+        '''
 
         if event.type == pygame.MOUSEBUTTONDOWN:
 
             #print (event.button)
 
             mouse_x, mouse_y = event.pos
+            
 
-            if button_pos[0] <= mouse_x <= button_pos[0] + button_size[0] and button_pos[1]+move <= mouse_y <= button_pos[1]+move + button_size[1]:
+            if scrollbar_button_pos[0] <= mouse_x <= scrollbar_button_pos[0] + scrollbar_button_size[0] and scrollbar_button_pos[1]+scrollbar_move <= mouse_y <= scrollbar_button_pos[1]+scrollbar_move + scrollbar_button_size[1]:
                 print('button be clicked')
-                moveable = True
+                scrollbar_moveable = True
             else:
-                moveable = False
+                scrollbar_moveable = False
 
         if event.type == pygame.MOUSEBUTTONUP:
 
-            moveable = False
+            scrollbar_moveable = False
 
         if event.type == MOUSEWHEEL:
             #print(event)
@@ -72,6 +85,6 @@ while True:
 
             mousewheel = event.y
 
-            move += mousewheel
+            scrollbar_move += mousewheel
 
             #print(event.y)
